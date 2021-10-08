7949cf865e6676ab1c021bfd61d501e3f8fdaa55
GouchinHub <69035493+GouchinHub@users.noreply.github.com>
2021-10-08 03:14:31 +0300

    alkaa olla jo toivotonta

--- routes/recipe.js
+++ routes/recipe.js
@@ -6,19 +6,32 @@ var instructions = ["heat", "eat"]
 var ingredients = ["flour", "meat"]
 
 var recipes = []
+var defaultRecipe = {
+  name
+}
 
 fs.readFile("./data/recipes.json", "utf-8", (err, data) => {
   if (err) {
     console.log(err)
     return
   }
+
   recipes = JSON.parse(data);
 })
 
 /* GET recipe by food. */
 router.get('/:food', function(req, res, next) {
   var items = recipes.filter(item => {return item.name === req.params.food})
-  res.json(items[0]);
+  if(items[0] != undefined || items[0] != null){
+    res.json(items[0]);
+  } else {
+    res.json({
+      name: req.params.food,
+      instructions: ["heat", "eat"],
+      ingridients: ["flour", "meat"]
+   });
+  }
+
 });
 
 /* POST recipes. */
